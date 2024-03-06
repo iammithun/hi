9ff93b1e91b161f99873410efb1cb68dfb043c36
iammithun <157653522+iammithun@users.noreply.github.com>
2024-03-05 22:13:59 -0500

    Add files via upload

---
+++ mithun.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 15 10:49:36 2023
+
+@author:mithun
+"""
+# this coding for Python Number
+# mithun
+x = 1 # int
+y = 2.0 #float
+z = 1j # complex
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1
+y = 35656222554887711
+z = -3255522
+print(type(x))
+print(type(y))
+print(type(z))
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+import random
+
+print(random.randrange(1, 10))
+x = 1 # int
+y = 2.8 # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
---
+++ modify.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 16 16:10:33 2023
+
+@author: mithun sai
+"""
+# this code about Modify string
+# this code by mithun sai 
+#UPPPER CASE
+a = "Hello, World!"
+print(a.upper())
+#LOWER CASE
+a = "Hello, World!"
+print(a.lower())
+#REMOVE WHITESPACE
+a = "Hello,World!"
+print(a.strip()) # returns "Hello, World!"
+#Replace String
+a = "Hello, World!"
+print(a.replace("H", "J"))
+#Split String
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+#this code about Modify string
+# this code by mithun sai 
+#UPPPER CASE
+a = "Hello, World!"
+print(a.upper())
+#LOWER CASE
+a = "Hello, World!"
+print(a.lower())
+#REMOVE WHITESPACE
+a = "Hello,World!"
+print(a.strip()) # returns "Hello, World!"
+#Replace String
+a = "Hello, World!"
+print(a.replace("H", "J"))
+#Split String
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
